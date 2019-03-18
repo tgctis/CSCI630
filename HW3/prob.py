16388f74781b6c1f4648dfe389e5967608f2f630
Timothy Chisholm <tgctis@rit.edu>
2019-03-18 11:06:01 -0400

    Finished HW3, didn't commit.

--- HW3/prob.py
+++ HW3/prob.py
@@ -12,7 +12,6 @@ rewards = [
 ]
 bet = 1
 wallet_size = 10
-simulations = 1000
 slots = 3
 probability = 0.25
 
@@ -89,19 +88,25 @@ def fselect(xs=[], i=0):
     return i
 
 
+simulations = 100
 total_pulls = 0
+fat_cat = 0
 all_pulls = []
 for i in range(1, simulations):
     wallet = wallet_size
     pulls = 0
     while wallet > 0:
         pulls = pulls + 1
-        wallet = wallet + reward(pull())
+        result = reward(pull())
+        if result > fat_cat:
+            fat_cat = result
+        wallet = wallet + result
         # print "Pull #%5d, Wallet $%5d" % (pulls, wallet)
     total_pulls = total_pulls + pulls
     all_pulls.append(pulls)
     # print "Simulation #%2d made %5d plays." %(i, pulls)
 
-print "%2d simulations: mean plays = %3d, median plays = %3d, max plays = %5d, min plays = %3d" \
+print "%2d simulations: mean plays = %3d, median plays = %3d" \
+      "\n, max plays = %5d, min plays = %3d, most coins = %5d" \
       % (simulations, total_pulls/simulations
-         , fselect(all_pulls, (math.floor(simulations/2))), max(all_pulls), min(all_pulls))
+         , fselect(all_pulls, (math.floor(simulations/2))), max(all_pulls), min(all_pulls), fat_cat)
