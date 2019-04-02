1bb53685900c67caa0bf44a1be522c43a94579f0
Timothy Chisholm <tgctis@rit.edu>
2019-04-02 19:07:10 -0400

    Final zip for HW4.... I hope.

--- HW4/perceptron.py
+++ HW4/perceptron.py
@@ -1,48 +1,82 @@
-import numpy as np
+import random
 
 texas = 1
 florida = 0
 
-alpha = 0.604
+# Learning rate
+alpha = 0.6
 
-x = np.array([[11, 70], [35, 11], [21, 45], [60, 80], [37, 32], [26, 64], [44, 30], [12, 60]])
-y = np.array([[1, 0, 1, 0, 0, 1, 0, 1]]).T
+# Data
+xs = [[11, 70], [35, 11], [21, 45], [60, 80], [37, 32], [26, 64], [44, 30], [12, 60]]
+ys = [1, 0, 1, 0, 0, 1, 0, 1]
+l = len(xs)
 
-synaptic_weights = np.random.random((2, 1))
-
-
-def f_activation(_z):
-    sum = 0
-    for e in _z:
-
-        if e > 30:
-            sum += 1
-        else:
-            sum -= 1
-    return sum/len(_z)
+# Weights
+ws = [0.1, 0.9]
+# ws = [random.random(), random.random()]
 
 
-for iteration in range(1):
-    z = np.dot(x, synaptic_weights)
-    activation = f_activation(z)
-    for _y in y:
-        if activation == _y:
-            continue
-
-        error = (_y - activation)
-        if activation > _y:
-            error = -1
-        else:
-            error = 1
-
-        synaptic_weights[0] += synaptic_weights[0] * alpha * error
-        synaptic_weights[1] += synaptic_weights[1] * alpha * error
+# step activation function
+def g(_sum):
+    if _sum/l > 1:
+        return 1
+    else:
+        return 0
 
 
-test_x1 = 44
-test_x2 = 30
+# Classify the G8rs
+def g8r(steps):
+    for step in range(steps):
+        for i in range(0, l):
+            _sum = 0
+            # Summation over all the x values & weights
+            for j in range(0, len(xs[i])):
+                _sum += ws[j] * xs[i][j]
 
-print("Consider the following data [" + str(test_x1) + ", " + str(test_x2) + "]")
-newZ = np.dot(np.array([test_x1, test_x2]), synaptic_weights)
-activationOutput = f_activation(newZ)
-print("%.5f" % activationOutput)
+            # calculated y
+            _y = g(_sum)
+
+            # check for accuracy
+            if _y > ys[i]:
+                error = -1
+            elif _y < ys[i]:
+                error = 1
+            else:
+                error = 0
+
+            # back propagate here
+            for j in range(0, len(xs[i])):
+                ws[j] += ws[j] * alpha * error
+
+
+# Does the final check of the data to see how the weights worked.
+def check(output=False):
+    total = l
+    right = 0
+    for _i in range(l):
+        test_x1 = xs[_i][0]
+        test_x2 = xs[_i][1]
+        actual = ys[_i]
+        computed = g(test_x1 * ws[0] + test_x2 * ws[1])
+        if computed == actual:
+            right += 1
+        if output:
+            print("Consider the following data [" + str(test_x1) + ", " + str(test_x2) + "] = " + str(actual)
+                  + " Computed: " + str(computed))
+    if output:
+        print "Total accuracy: %.2f%%" % ((right * 1.0/total * 1.0) * 100)
+    return right
+
+
+_steps = 1
+MAX_STEPS = 10000
+g8r(_steps)
+check()
+
+while check() < l and _steps < MAX_STEPS:
+    _steps += 1
+    g8r(_steps)
+
+
+check(True)
+print "Final weights: ", ws, "Num steps: ", _steps
