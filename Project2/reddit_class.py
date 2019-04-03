d31b3e4049755ab6bffb0b643aa4ad6cc90c8139
Timothy Chisholm <tgctis@rit.edu>
2019-04-02 21:25:45 -0400

    Data gathered-ish, intro steps taken to make a bag of words.

--- Project2/reddit_class.py
+++ Project2/reddit_class.py
@@ -1,23 +1,49 @@
 import sys
 import os
-sys.path.append(os.path.abspath("C:\\Users\\tchisholm\\.PyCharm2018.3\\config\\scratches\\"))
+import time
+import pandas as pd
+import hashlib
+sys.path.append(os.path.abspath("C:\\tmp"))
 from reddit_id import *
 
 reddit = get_reddit()
-submission_num = 0
-separator = '----------------------------------------------------------------------'
 
 
-for submission in reddit.subreddit('legaladvice').stream.submissions():
-	submission_num = submission_num + 1
-	if submission_num > 10:
-		exit(0)
-	submission.comments.replace_more(limit=None)
-	for comment in submission.comments.list():
-		# print(comment.body)
-		# print(comment.name)
-		# print(comment.author)
-		if str(comment.author) == 'LocationBot':
-			continue
-		print(separator + '\n' + 'AUTHOR: ' + str(comment.author) + '\n' + comment.body + '\n' + separator + '\n')
+def load_df(_subreddit):
+	return pd.read_csv(_subreddit + ".csv")
 
+
+def word_sack(_df):
+	return _df.apply(lambda x: x.str.split(expand=True).stack()).stack().value_counts()
+
+def cleave(_subreddit, MAX_POSTS):
+	timestamp = time.time()
+	postlist = []
+
+	submission_num = 0
+	for submission in reddit.subreddit(_subreddit).stream.submissions():
+		submission_num = submission_num + 1
+		if submission_num > MAX_POSTS:
+			break
+		print "Submission #", submission_num
+		submission.comments.replace_more(limit=None)
+		for comment in submission.comments.list():
+			hash_object = hashlib.md5(comment.permalink)
+			id = hash_object.hexdigest()
+			post = [id, comment.body, _subreddit]
+			postlist.append(post)
+
+	print "Posts: ", len(postlist)
+	df = pd.DataFrame(postlist, columns=['ID', 'BODY', 'SUBREDDIT'])
+	df.to_csv(_subreddit + '.csv', index=False, mode='w', encoding="utf-8")
+
+
+def workit(posts, subreddits):
+	for subreddit in subreddits:
+		# cleave(subreddit, posts)
+		df = load_df(subreddit)
+		bag_of_words = word_sack(df)
+		print bag_of_words
+
+
+workit(100, ['legaladvice', 'politics'])
